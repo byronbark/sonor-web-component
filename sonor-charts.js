d7c8282b8bcb22caae39b1072b93235b328adb34
Byron Barkhuizen <bbarkhuizen@matrice.io>
2021-07-09 08:53:12 +0200

    Update html

--- sonor-charts.js
+++ sonor-charts.js
@@ -15,6 +15,7 @@ export class SonorCharts extends HTMLElement {
         var newChart;
         var isClicked=false;
         var myLine;
+        var switched=false;
         
         const shadowRoot = this.attachShadow({ mode: 'open' });
 
@@ -28,15 +29,39 @@ export class SonorCharts extends HTMLElement {
 
         //binding methods
         this.increaseZoom=this.increaseZoom.bind(this);
+        this.demoMap=this.demoMap.bind(this);
         this.decreaseZoom=this.decreaseZoom.bind(this);
         this.updateLayer=this.updateLayer.bind(this);
         this.initMap=this.initMap.bind(this);
-        this.addInitialLayers=this.addInitialLayers.bind(this)
+        this.addInitialLayers=this.addInitialLayers.bind(this);
+        this.addInitialLayers=this.addInitialLayers.bind(this);
+        this.updateLayers=this.updateLayers.bind(this);
+        this.updateLayerTwo=this.updateLayerTwo.bind(this);
+        this.initGraph=this.initGraph.bind(this);
+
+
+
     }
     
 
     updateLayer(e) {
         console.log('updated');
+        this.shadowRoot.getElementById('input-3').setAttribute('class','btn btn-primary mr-1');
+        this.shadowRoot.getElementById('input-2').setAttribute('class','btn btn-secondary mr-1');
+        this.removeLayers(this.map);
+        
+        console.log('layers updated');
+
+        if (this.switched) {
+            this.updateLayers(this.map);
+        } else {
+            this.addInitialLayers(this.map);
+        }
+
+    }
+
+
+    removeLayers(e) {
         if (this.map.getLayer('pointLayer')) {
             this.map.removeLayer('pointLayer');
           }
@@ -49,11 +74,24 @@ export class SonorCharts extends HTMLElement {
         if (this.map.getSource('hexGrid')) {
             this.map.removeSource('hexGrid');
           }
+    }
+    
+    updateLayerTwo(e) {
+        if (!this.switched) {
+            this.shadowRoot.getElementById("input-4").innerText="Évolution";
+            this.switched=true;
+            this.removeLayers(this.map);
+            this.updateLayers(this.map);
+        } else {
+            this.shadowRoot.getElementById("input-4").innerText="Moyenne";
+            this.switched=false;
+            this.removeLayers(this.map);
+            this.addInitialLayers(this.map);
+
+        }
+        console.log('updated');
         
         console.log('layers updated');
-
-
-        this.addInitialLayers(this.map);
     }
 
     increaseZoom(e) {
@@ -71,32 +109,71 @@ export class SonorCharts extends HTMLElement {
     }
 
     decreaseZoom(e) {
+        this.shadowRoot.getElementById('input-2').setAttribute('class','btn btn-primary mr-1');
+        this.shadowRoot.getElementById('input-3').setAttribute('class','btn btn-secondary mr-1');
         console.log('clicked 2');
-        console.log(this.map);
-        this.map.zoomTo(8);
+        this.removeLayers(this.map);
+        this.addInitialLayers(this.map);
     }
 
-    initGraph() {
+    demoMap(e) {
+        var loaded=this.map.areTilesLoaded();
+        console.log(loaded);
+        if (loaded) {
+        this.map.flyTo({
+            // These options control the ending camera position: centered at
+            // the target, at zoom level 9, and north up.
+            center: [2.349014, 48.864716],
+            zoom: 13,
+            bearing: 10,
+             
+            // These options control the flight curve, making it move
+            // slowly and zoom out almost completely before starting
+            // to pan.
+            speed: 0.2, // make the flying slow
+            curve: 1, // change the speed at which it zooms out
+             
+            // This can be any easing function: it takes a number between
+            // 0 and 1 and returns another number between 0 and 1.
+            easing: function (t) {
+            return t;
+            },
+             
+            // this animation is considered essential with respect to prefers-reduced-motion
+            essential: true
+            });
+            setTimeout(function() {this.removeLayers(this.map);this.updateLayers(this.map);}.bind(this),3000);
+        }
+    }
+
+    initGraph(isFirst) {
+
         var config = {
             type: 'line',
             data: {
                 labels: ['Dimanche','Lundi', 'Mardi', 'Mercredi', 'Aujourdhui'],
                 datasets: [{
                     label: 'Bruit Nocturnale (dB)',
-                    backgroundColor: "#E5291D",
-                    borderColor: "#E5291D",
+                    backgroundColor: "rgba(255,159,5,0.3)",
+                    borderColor: "rgba(255,159,5,0.6)",
+                    pointBackgroundColor: "rgba(255,159,5,0.9)",
+                    tension: 0.2,
                     data: [34,49,48,60,45],
                     fill: true,
                 }, {
                     label: 'Bruit Jour (dB)',
-                    backgroundColor: "hsl(37,100%,51%)",
-                    borderColor: "hsl(37,100%,51%)",
+                    backgroundColor: "rgba(229,41,29,0.3)",
+                    borderColor: "rgba(229,41,29,0.6)",
+                    pointBackgroundColor: "rgba(229,41,29,0.9)",
+                    tension: 0.2,
                     fill: true,
-
                     data: [42,49,35,75,32],
                 }]
             },
             options: {
+                animation: {
+                    duration: 4500,
+                },
                 responsive: true,
                 title: {
                     display: true,
@@ -121,45 +198,64 @@ export class SonorCharts extends HTMLElement {
                         display: true,
                         scaleLabel: {
                             display: true,
-                            labelString: ''
+                            labelString: 'Bruit (dB)'
                         }
                     }],
                     yAxes: [{
                         display: true,
                         scaleLabel: {
                             display: true,
-                            labelString: ''
+                            labelString: 'Jour'
                         }
                     }]
                 }
             }
         };
 
-
-        var canvas = this.shadowRoot.getElementById('canvas');
-        if (!canvas){
+            var canvas = this.shadowRoot.getElementById('canvas');
+            if (!canvas){
             console.log("CANVAS EMPTY");
         }
             
         var ctx = canvas.getContext('2d');
         window.myLine = new Chart(ctx, config);
+
+        setInterval(function(){
+            // Get a random index point
+            var indexToUpdate = Math.round(Math.random() * config.data.labels.length);
+            console.log(indexToUpdate)
+            var avg=myLine.data.datasets[0].data.reduce((acc,v) => acc + v) / config.data.labels.length;
+            this.shadowRoot.getElementById("canvasResult").innerHTML="Moyenne:"+avg;
+            // Update one of the points in the second dataset
+            //myLine.datasets[1].points[indexToUpdate].value = Math.random() * 100;
+            myLine.data.datasets[0].data[indexToUpdate]=Math.floor(Math.random()*100);
+            myLine.data.datasets[1].data[indexToUpdate]=Math.floor(Math.random()*50);
+
+            
+            myLine.update();
+          }.bind(this), 5000);
     }
 
     connectedCallback() {
         //initialise map and add layers after loaded into DOM
         this.initMap();
         this.addInitialLayers(this.map);
-        this.initGraph();
+        this.initGraph(true);
 
         //inputs and controls
-        const zoominbutton = this.shadowRoot.getElementById('input-1');
+        const demobutton = this.shadowRoot.getElementById('input-1');
         const zoomoutbutton = this.shadowRoot.getElementById('input-2');
         const updatelayerbutton = this.shadowRoot.getElementById('input-3');
+        const updatelayertwobutton = this.shadowRoot.getElementById('input-4');
+
 
         //setup events
-        zoominbutton.addEventListener('click',this.increaseZoom);
+        demobutton.addEventListener('click',this.demoMap);
         zoomoutbutton.addEventListener('click',this.decreaseZoom);
         updatelayerbutton.addEventListener('click',this.updateLayer);
+        updatelayertwobutton.addEventListener('click',this.updateLayerTwo);
+        console.log(this.switched);
+
 
         
 
@@ -177,6 +273,7 @@ export class SonorCharts extends HTMLElement {
             zoom: 14 // starting zoom
         });
         this.map.addControl(new mapboxgl.NavigationControl());
+
         
         //On hex click event
 
@@ -187,6 +284,78 @@ export class SonorCharts extends HTMLElement {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
     }
+
+    updateLayers(mapbox,e) {
+        // Loading hexagon data with d3
+        d3.json(hexurl,
+            function(err, data) {
+            
+            if (err) throw err;
+            data.features = data.features.map(function (d) {
+                d.properties.scale = getRandomInt(35,85);
+                return d;
+            });
+
+
+            console.log(data)
+
+            mapbox.addSource('hexGrid', {
+                type: 'geojson',
+                'data': data,
+            });
+            
+            mapbox.addLayer({
+                id: 'hexLayer',
+                type: 'fill',
+                source: 'hexGrid',
+                'layout': {},
+                'paint': {
+                    'fill-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['get', 'scale'],
+                        30,
+                        '#d3f9b5',
+                        35,
+                        'hsl(199,90%,83%)',
+                        50,
+                        'hsl(200,73%,66%)',
+                        70,
+                        'hsl(199,90%,83%)',
+                        80,
+                        'red'
+                        ],
+                    'fill-opacity': 0.7,
+                    'fill-outline-color': "rgba(0,0,0,0)"
+                }
+            });
+
+
+        });
+
+        d3.json(pointsurl,
+            function(err, data) {
+            
+            if (err) throw err;
+
+            mapbox.addSource('points', {
+                type: 'geojson',
+                'data': data,
+            });
+            
+            mapbox.addLayer({
+                id: 'pointLayer',
+                type: 'circle',
+                source: 'points',
+                'paint': {
+                    'circle-color': "rgb(5,5,5)",
+                    'circle-radius': 10
+                }
+            });
+
+
+        });
+}
     
 
     addInitialLayers(mapbox,e) {
--- template.js
+++ template.js
@@ -1,29 +1,30 @@
 export const templateContent = `
-    <div id="headerbar" class="block-multi-item bg-black text-white d-flex flex-column justify-content-start flex-xl-row align-items-left p-1 position-relative">
-        <btn id="input-1" class="btn btn-secondary text-black mr-1">Demo</btn>
+    <div id="headerbar" class="block-multi-item bg-black text-white flex-column justify-content-start flex-xl-row align-items-left p-1 position-relative">
+        <btn id="input-1" class="btn btn-secondary mr-1">Demo</btn>
         <btn id="input-2" class="btn btn-secondary mr-1">J1</btn>
         <btn id="input-3" class="btn btn-secondary mr-1">J7</btn>
-        <div class="dropdown mb-0">
-            <button class="btn btn-secondary btn-icon-left dropdown-toggle" type="button" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-               Évolution
-            </button>
-        </div>
+        <btn id="input-4" class="btn btn-secondary mr-1">Moyenne</btn>
     </div>
-    <div class="row">
+    <div class="row mb-2">
         <div class="col">
             <div id='map' style='width: 100%; height: 400px;'>
             </div>
         </div>
-        <div class="col-md-4 mb-0 pt-0">
-            <div class="card pt-10">
-                <canvas id="canvas"></canvas>
-            </div>
+        <div class="col col-md-4 ">
+                <div class="card justify-content-end align-items-stretch">
+                    <canvas id="canvas"></canvas>
+                    <div class="alert alert-warning" role="alert">
+                        <span class="icon-information_outline text-info"></span>
+                        <span id="canvasResult"></span>
+                    </div>
+                </div>
         </div>
     </div>
     <div class="container pl-0 pr-0" id="template-body">
         <span class="font-weight-bold font-size-36 mb-5 mt-5">
         <i class="icon icon-border-top-left-3 font-size-28 btn-link ml-2 mb-2"></i>
             Sonor Dashboard
+        <i class="icon icon-maintien_investissement_production_neuve_et_rehabilitation_thermique font-size-28"></i>
         </span>
         </div>
     </div>
